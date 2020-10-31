3a5ec38cb9c22f4cf2182c83c00d1977a4aa9de5
xuefivefour <xuefivefour@gmail.com>
2020-10-30 19:12:06 -0500

    Redefine functions in __init__ method as methods; Create PlotWindow class

--- 107_GUI.py
+++ 107_GUI.py
@@ -19,28 +19,31 @@ class MplCanvas(FigureCanvasQTAgg):
         self.axes = fig.add_subplot(111)
         super(MplCanvas, self).__init__(fig)
 
-class TestPlot(QMainWindow):
+class PlotWindow(QMainWindow):
 
     def __init__(self, *args, **kwargs):
-        super(TestPlot, self).__init__(*args, **kwargs)
+        super(PlotWindow, self).__init__(*args, **kwargs)
         
-        sc = MplCanvas(self, width=5, height=4, dpi=100)
-        sc.axes.plot([0,1,2,3,4], [1,2,3,2,5])
+    def plot(self):
+        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
         
-        # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
-        toolbar = NavigationToolbar(sc, self)
-
+        toolbar = NavigationToolbar(self.sc, self)
         layout = QVBoxLayout()
         layout.addWidget(toolbar)
-        layout.addWidget(sc)
-
-        # Create a placeholder widget to hold our toolbar and canvas.
+        layout.addWidget(self.sc)
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
     
-# hello test comment for git
+app= QApplication([])
+window = PlotWindow()
+window.plot()
+window.sc.axes.plot([0,1,2,3,4], [1,2,3,2,5])
+window.show()
+app.exec_()
 
+
+       
 class SinglePhasePlot(QGroupBox):
     
     def __init__(self, *args, **kwargs):
@@ -48,82 +51,83 @@ class SinglePhasePlot(QGroupBox):
         
         self.setTitle("Single Phase Plots")
         
-        filebutton = QPushButton("Choose File")
-        filelabel = QLabel("")
+        self.filebutton = QPushButton("Choose File")
+        self.filelabel = QLabel("")
         
         filelayout = QHBoxLayout()
-        filelayout.addWidget(filebutton)
-        filelayout.addWidget(filelabel) 
+        filelayout.addWidget(self.filebutton)
+        filelayout.addWidget(self.filelabel) 
     
-        coolbutton = QRadioButton("Cooldown/Warmup Phase")
-        regenbutton = QRadioButton("Regeneration Phase")
-        regbutton = QRadioButton("Regulation Phase")
-        coolbutton.setChecked(True)
+        self.coolbutton = QRadioButton("Cooldown/Warmup Phase")
+        self.regenbutton = QRadioButton("Regeneration Phase")
+        self.regbutton = QRadioButton("Regulation Phase")
         
         buttonlayout = QVBoxLayout()
-        buttonlayout.addWidget(coolbutton)
-        buttonlayout.addWidget(regenbutton)
-        buttonlayout.addWidget(regbutton)
+        buttonlayout.addWidget(self.coolbutton)
+        buttonlayout.addWidget(self.regenbutton)
+        buttonlayout.addWidget(self.regbutton)
         
-        chooselabel = QLabel("Choose phase: ")
-        choosephase = QComboBox()
+        self.chooselabel = QLabel("Choose phase: ")
+        self.choosephase = QComboBox()
         
         chooselayout = QHBoxLayout()
-        chooselayout.addWidget(chooselabel)
-        chooselayout.addWidget(choosephase)
+        chooselayout.addWidget(self.chooselabel)
+        chooselayout.addWidget(self.choosephase)
         
-        plot = QPushButton("Plot")
+        self.plot = QPushButton("Plot")
         
         layout = QVBoxLayout()
         layout.addLayout(filelayout)
         layout.addLayout(buttonlayout)
         layout.addLayout(chooselayout)
-        layout.addWidget(plot)
+        layout.addWidget(self.plot)
         self.setLayout(layout)
         
-        def open_file():
-            path = QFileDialog.getOpenFileName(window, "Open")[0]
-            if path:
-                global logs
-                filelabel.setText(path[0:15] + '...' + path[-25:]) 
-                logs = split_107(load_107(path))
+        self.filebutton.clicked.connect(self.open_file)
+        self.coolbutton.pressed.connect(self.press_cool)
+        self.regenbutton.pressed.connect(self.press_regen)
+        self.regbutton.pressed.connect(self.press_reg) 
+        self.choosephase.activated.connect(self.choose_phase)
+        self.plot.clicked.connect(self.show_plot)
+    
+    def open_file(self):
+        path = QFileDialog.getOpenFileName(window, "Open")[0]
+        if path:
+            global logs
+            self.filelabel.setText(path[0:15] + '...' + path[-25:]) 
+            logs = split_107(load_107(path))
         
         #the following 3 functions are repetitive. is there a way to connect all three buttons to the same function
         #which then uses a conditional statement based on which button is pressed? is there a way to tell which button is pressed?
-        def press_cool():
-            options = ["cooldown", "warmup"]
-            choosephase.clear()
-            choosephase.addItems(options)
-            global phasetype
-            phasetype = 0
+    def press_cool(self):
+        options = ["cooldown", "warmup"]
+        self.choosephase.clear()
+        self.choosephase.addItems(options)
+        global phasetype
+        phasetype = 0
         
-        def press_regen():
-            options = list(logs[1].keys())
-            choosephase.clear()
-            choosephase.addItems(options)
-            phasetype = 1 
+    def press_regen(self):
+        options = list(logs[1].keys())
+        self.choosephase.clear()
+        self.choosephase.addItems(options)
+        phasetype = 1 
             
-        def press_reg():
-            options = list(logs[2].keys())
-            choosephase.clear()
-            choosephase.addItems(options)
-            phasetype = 2
+    def press_reg(self):
+        options = list(logs[2].keys())
+        self.choosephase.clear()
+        self.choosephase.addItems(options)
+        phasetype = 2
         
-        def choose_phase():
-            phaseindex = choosephase.currentIndex()
-            print(phaseindex)
+    def choose_phase(self):
+        phaseindex = self.choosephase.currentIndex()
+        print(phaseindex)
             
-        def show_plot():
-            self.plot = TestPlot()
-            self.plot.show()
+    def show_plot(self):
+        self.plotwindow = TestPlot()
+        self.plotwindow.show()
         
-        filebutton.clicked.connect(open_file)
-        coolbutton.pressed.connect(press_cool)
-        regenbutton.pressed.connect(press_regen)
-        regbutton.pressed.connect(press_reg) 
-        choosephase.activated.connect(choose_phase)
-        plot.clicked.connect(show_plot)
         
+'''       
 
 app = QApplication(sys.argv)
 
@@ -132,7 +136,7 @@ window.show()
 
 app.exec_() 
 
-
+'''
 
 
 
