3ca6579038008acbe9a599a63c82d971ccb06f69
Tejas Guruswamy <tguruswamy@anl.gov>
2020-11-24 14:44:00 -0600

    Add guards around application start

--- GUI_107.py
+++ GUI_107.py
@@ -386,15 +386,15 @@ class MainWindow(QMainWindow):
         widget.setLayout(layout)
         
         self.setCentralWidget(widget)
-        
-        
-   
-app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
 
-app.exec_() 
+if __name__ == "__main__": 
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec_() 
 
 
 
